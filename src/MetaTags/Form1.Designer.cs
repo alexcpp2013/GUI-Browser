c60278fb3482aaedb4262683a7b8645aa80cbab8
alexcpp2013 <alex.melnik@iua>
2014-02-24 18:52:26 +0200

    First Commit

---
+++ src/MetaTags/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace MetaTags
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.goButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.Web = new System.Windows.Forms.WebBrowser();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripTextBox1 = new System.Windows.Forms.ToolStripTextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.goButton);
+            this.panel1.Controls.Add(this.exitButton);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 438);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(784, 42);
+            this.panel1.TabIndex = 1;
+            // 
+            // goButton
+            // 
+            this.goButton.Dock = System.Windows.Forms.DockStyle.Left;
+            this.goButton.Location = new System.Drawing.Point(0, 0);
+            this.goButton.Name = "goButton";
+            this.goButton.Size = new System.Drawing.Size(86, 42);
+            this.goButton.TabIndex = 0;
+            this.goButton.Text = "Определить";
+            this.goButton.UseVisualStyleBackColor = true;
+            this.goButton.Click += new System.EventHandler(this.goButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.exitButton.Dock = System.Windows.Forms.DockStyle.Right;
+            this.exitButton.Location = new System.Drawing.Point(709, 0);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 42);
+            this.exitButton.TabIndex = 1;
+            this.exitButton.Text = "Выйти";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.Web);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(784, 438);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Данные";
+            // 
+            // Web
+            // 
+            this.Web.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Web.Location = new System.Drawing.Point(3, 16);
+            this.Web.MinimumSize = new System.Drawing.Size(20, 20);
+            this.Web.Name = "Web";
+            this.Web.Size = new System.Drawing.Size(778, 419);
+            this.Web.TabIndex = 0;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1,
+            this.toolStripSeparator1,
+            this.toolStripTextBox1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(784, 25);
+            this.toolStrip1.TabIndex = 4;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(42, 22);
+            this.toolStripLabel1.Text = "Адрес:";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // toolStripTextBox1
+            // 
+            this.toolStripTextBox1.AutoCompleteCustomSource.AddRange(new string[] {
+            "http://kpi.ua",
+            "http://google.com.ua"});
+            this.toolStripTextBox1.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.toolStripTextBox1.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.AllUrl;
+            this.toolStripTextBox1.AutoToolTip = true;
+            this.toolStripTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.toolStripTextBox1.Font = new System.Drawing.Font("Tahoma", 11F);
+            this.toolStripTextBox1.MaxLength = 2500;
+            this.toolStripTextBox1.Name = "toolStripTextBox1";
+            this.toolStripTextBox1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.toolStripTextBox1.Size = new System.Drawing.Size(500, 25);
+            this.toolStripTextBox1.ToolTipText = "Введите адрес, например: http://kpi.ua";
+            this.toolStripTextBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.toolStripTextBox1_KeyDown);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(559, 0);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(213, 24);
+            this.progressBar1.Step = 1;
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this.progressBar1.TabIndex = 5;
+            this.progressBar1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.goButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.exitButton;
+            this.ClientSize = new System.Drawing.Size(784, 480);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel1);
+            this.DoubleBuffered = true;
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Определение Мета-Тегов";
+            this.panel1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripTextBox toolStripTextBox1;
+        private System.Windows.Forms.Button goButton;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.WebBrowser Web;
+
+    }
+}
+
---
+++ src/MetaTags/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading.Tasks;
+using mshtml;
+using System.Xml.Linq;
+using System.Net;
+
+namespace MetaTags
+{
+    public partial class Form1 : Form
+    {
+        //private WebBrowser Web = null;
+        private XDocument xml;
+        private WebClient client = new WebClient();
+
+        private enum TypeParameter { Good, Nothing, Error}
+
+        private static class HttpData
+        {
+            public static string http = "http://";
+            public static string https = "https://";
+            public static string www = "www.";
+            //ww2 ww3 www2 www3 ... must use in address
+            //or use List<string>
+
+            public enum TypeParameter { Good, Nothing, Error }
+
+            public static string Culture = "en-US";
+
+            //public const int TREE = 0;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            StartNewBrowser();
+        }
+
+        private bool Navigate(String address)
+        {
+            //Use user address for understand what site we loaded exactly
+            if (String.IsNullOrEmpty(address)) return false;
+            if (address.Equals("about:blank")) return false;
+            if (!address.StartsWith(HttpData.http) &&
+                !address.StartsWith(HttpData.https))
+            {
+                address = HttpData.http + address;
+            }
+
+            try
+            {
+                Web.Navigate(new Uri(address));
+                return true;
+            }
+            catch (System.UriFormatException ex)
+            {
+                throw new Exception("Ошибка в uri. \n" + ex.Message);
+                //return false;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Ошибка при переходе к документу. \n" + ex.Message);
+                //return false;
+            }
+
+        }
+
+        private void Manage()
+        {
+            string url = toolStripTextBox1.Text;
+            
+            if (url == "")
+            {
+                MessageBox.Show("Указан пустой url.", "Информационное сообщение",
+                                MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            //StartNewBrowser();
+
+            LoadPage(url);
+        }
+
+        private void LoadPage(string url)
+        {
+            try
+            {
+                progressBar1.Visible = true;
+
+                if (Navigate(url) != true)
+                    throw (new Exception("Не корректный url."));
+
+                while (Web.ReadyState != WebBrowserReadyState.Complete || Web.IsBusy)
+                    //|| Web.Url.AbsoluteUri != url)
+                    Application.DoEvents();
+
+                if (Web.Document == null)
+                    throw new Exception("Страница не загрузилась.");
+
+                IHTMLDocument2 currentDoc = (IHTMLDocument2)Web.Document.DomDocument;
+                if (currentDoc.url.StartsWith("res://"))
+                    throw new Exception("Не найдена страница: " + url + "\n");
+
+                if (Web.Document.Body == null)
+                    throw new Exception("Документ не заргружен.");
+
+                //progressBar1.Visible = false;
+
+            }
+            catch (Exception err)
+            {
+                progressBar1.Visible = false;
+                MessageBox.Show("Произошла ошибка во время работы программы: \n\n" +
+                                err.Message, "Ошибка",
+                                MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                progressBar1.Visible = false;
+            }
+        }
+
+        private void StartNewBrowser()
+        {
+            ClearWebBrowser();
+
+            //Web = new WebBrowser();
+            Web.ScriptErrorsSuppressed = true;
+            Web.Visible = true;
+        }
+
+        private void ClearWebBrowser()
+        {
+            /*if(Web != null)
+                Web.Dispose();
+            Web = null;*/
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            ClearAll();
+            Close();
+        }
+
+        private void ClearAll()
+        {
+            progressBar1.Visible = false;
+            ClearWebBrowser();
+            toolStripTextBox1.Clear();
+        }
+
+        private void goButton_Click(object sender, EventArgs e)
+        {
+            Manage();
+        }
+
+        private void toolStripTextBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                Manage();
+            }
+        }
+    }
+}
